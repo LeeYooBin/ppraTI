4d1d10b567ad8f4b505b2fd4c52bef3a52b97601
LeeYooBin <santosroberdan25@gmail.com>
2024-08-29 23:57:24 -0300

    config: installing and setting tailwindcss and postcss

--- multi-app/src/main.jsx
+++ multi-app/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "./global.css";
 import { BrowserRouter as Router } from 'react-router-dom'
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
 
@@ -9,5 +10,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
