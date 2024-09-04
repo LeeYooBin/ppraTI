47a3fb7564d728cd5278cb16b28740c9d0ba9057
LeeYooBin <santosroberdan25@gmail.com>
2024-09-04 11:52:50 -0300

    fix: adjusting not-found page style to adapt to mobile

--- netflix-clone/src/pages/not-found.jsx
+++ netflix-clone/src/pages/not-found.jsx
@@ -13,13 +13,13 @@ export const NotFound = () => (
       }}
     >
       <div className="flex flex-col items-center justify-center gap-8 w-[100%] min-h-[80vh] text-white">
-        <h1 className="text-9xl font-bold">Are you lost?</h1>
-        <p className="text-5xl font-thin text-center w-[50%]">
+        <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold">Are you lost?</h1>
+        <p className="text-xl md:text-3xl lg:text-5xl font-thin text-center w-[80%] md:w-[50%]">
           Unfortunately, we were unable to locate this page. You can find many other titles on our home page.
         </p>
         <Link to="/">
           <Button 
-            className="bg-white border-none text-black"
+            className="bg-white border-none text-black shadow-lg"
             size="lg"
           >
             Netflix Homepage
