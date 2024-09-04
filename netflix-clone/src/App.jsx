db41ed83e04d9072d4653dedb8c66a11514a46f5
LeeYooBin <santosroberdan25@gmail.com>
2024-09-03 22:20:26 -0300

    feat: adding react-router-dom and setting the app routes

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,5 +1,16 @@
-export const App = () => {
-  return (
-    <h1 className="text-blue-500">Hello world!</h1>
-  );
-}
+import { 
+  BrowserRouter as Router, 
+  Routes, 
+  Route
+} from "react-router-dom";
+import { Home } from "./pages/home";
+import { NotFound } from "./pages/not-found";
+
+export const App = () => (
+  <Router>
+    <Routes>
+      <Route path="*" element={<NotFound />}></Route>
+      <Route path="/" element={<Home />}></Route>
+    </Routes>
+  </Router>
+);
