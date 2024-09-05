ec976f96ed13ae45eefebe77d8714fb434be2c50
LeeYooBin <santosroberdan25@gmail.com>
2024-09-05 15:34:26 -0300

    feat: setting all application routes

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -4,6 +4,10 @@ import {
   Route
 } from "react-router-dom";
 import { Home } from "./pages/home";
+import { Movies } from "./pages/movies";
+import { Series } from "./pages/series";
+import { Trending } from "./pages/trending";
+import { Genre } from "./pages/genre";
 import { NotFound } from "./pages/not-found";
 
 export const App = () => (
@@ -11,6 +15,10 @@ export const App = () => (
     <Routes>
       <Route path="*" element={<NotFound />}></Route>
       <Route path="/" element={<Home />}></Route>
+      <Route path="/movies" element={<Movies />}></Route>
+      <Route path="/series" element={<Series />}></Route>
+      <Route path="/trending" element={<Trending />}></Route>
+      <Route path="/genres/:genreId" element={<Genre />}></Route>
     </Routes>
   </Router>
 );
